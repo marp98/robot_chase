d55a667a88b5395a2ee1f74489211c0bafe81eeb
Manuel Roshardt <manuelroshardt@gmail.com>
2024-04-24 15:22:08 +0000

    Updated chase logic so that rick stops once it bumps into morty

--- src/robot_chase.cpp
+++ src/robot_chase.cpp
@@ -7,50 +7,55 @@ using namespace std::chrono_literals;
 
 class RobotChaseNode : public rclcpp::Node
 {
-    public:
-        RobotChaseNode() : Node("robot_chase_node")
-        {
-            publisher_ = create_publisher<geometry_msgs::msg::Twist>("rick/cmd_vel", 10);
-            tf_buffer_ = std::make_unique<tf2_ros::Buffer>(get_clock());
-            transform_listener_ = std::make_unique<tf2_ros::TransformListener>(*tf_buffer_);
-            timer_ = create_wall_timer(100ms, std::bind(&RobotChaseNode::controlLoop, this));
+public:
+    RobotChaseNode() : Node("robot_chase_node")
+    {
+        publisher_ = create_publisher<geometry_msgs::msg::Twist>("rick/cmd_vel", 10);
+        tf_buffer_ = std::make_unique<tf2_ros::Buffer>(get_clock());
+        transform_listener_ = std::make_unique<tf2_ros::TransformListener>(*tf_buffer_);
+        timer_ = create_wall_timer(100ms, std::bind(&RobotChaseNode::controlLoop, this));
+        kp_distance_ = 0.5;
+        kp_yaw_ = 1.0;
+        distance_threshold_ = 0.4;  
+    }
 
-            kp_distance_ = 0.5;
-            kp_yaw_ = 1.0;
+private:
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    std::unique_ptr<tf2_ros::TransformListener> transform_listener_;
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    double kp_distance_;
+    double kp_yaw_;
+    double distance_threshold_;  
+
+    void controlLoop()
+    {
+        geometry_msgs::msg::TransformStamped transform;
+        try
+        {
+            transform = tf_buffer_->lookupTransform("rick/base_link", "morty/base_link", tf2::TimePointZero);
+        }
+        catch (const tf2::TransformException& ex)
+        {
+            RCLCPP_ERROR(get_logger(), "Could not get transform: %s", ex.what());
+            return;
         }
 
-    private:
-        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-        std::unique_ptr<tf2_ros::TransformListener> transform_listener_;
-        std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-        rclcpp::TimerBase::SharedPtr timer_;
+        double error_distance = sqrt(pow(transform.transform.translation.x, 2) + pow(transform.transform.translation.y, 2));
+        double error_yaw = atan2(transform.transform.translation.y, transform.transform.translation.x);
+        double angular_velocity = kp_yaw_ * error_yaw;
+        double linear_velocity = 0.0;
 
-        double kp_distance_;
-        double kp_yaw_;
+        if (error_distance > distance_threshold_)
+        {
+            linear_velocity = kp_distance_ * error_distance;
+        }
 
-        void controlLoop()
-            {
-                geometry_msgs::msg::TransformStamped transform;
-                try {
-                    transform = tf_buffer_->lookupTransform("rick/base_link", "morty/base_link", tf2::TimePointZero);
-                }
-                catch (const tf2::TransformException& ex) {
-                    RCLCPP_ERROR(get_logger(), "Could not get transform: %s", ex.what());
-                    return;
-                }
-
-                double error_distance = sqrt(pow(transform.transform.translation.x, 2) + pow(transform.transform.translation.y, 2));
-                double error_yaw = atan2(transform.transform.translation.y, transform.transform.translation.x);
-
-                double angular_velocity = kp_yaw_ * error_yaw;
-                double linear_velocity = kp_distance_ * error_distance;
-
-                geometry_msgs::msg::Twist twist;
-                twist.linear.x = linear_velocity;
-                twist.angular.z = angular_velocity;
-
-                publisher_->publish(twist);
-            }
+        geometry_msgs::msg::Twist twist;
+        twist.linear.x = linear_velocity;
+        twist.angular.z = angular_velocity;
+        publisher_->publish(twist);
+    }
 };
 
 int main(int argc, char** argv)
